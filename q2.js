a6757e67fab0d2eb88cd0a9b29107be50618a45c
maayantav <77383039+maayantav@users.noreply.github.com>
2021-01-18 14:16:34 +0200

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,13 @@
+let num1 = 5, num2 = 9, num3 = -32;
+let product = 0;
+
+product = num1 * num2 * num3;
+if(product < 0)
+{
+    alert("The sign is -");
+}
+
+else if (product > 0)
+{
+    alert("The sign is +");
+}
