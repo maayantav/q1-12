cb6fbc73dfe07d4329051825df1f377cd9a5199a
maayantav <77383039+maayantav@users.noreply.github.com>
2021-01-18 14:55:16 +0200

    Add files via upload

---
+++ q3.js
@@ -0,0 +1,35 @@
+let num1 = 5, num2 = 9, num3 = -32;
+
+if (num1 < num2 && num1 < num3){
+    
+    if(num2 < num3)
+    {
+        console.log(num3 + ", " + num2 + ", " + num1);
+    }
+    else
+    {
+        console.log(num2 + ", " + num3 + ", " + num1);
+    }
+}
+else if (num2 < num1 && num2 < num3){
+    
+    if(num1 < num3)
+    {
+        console.log(num3 + ", " + num1 + ", " + num2);
+    }
+    else
+    {
+        console.log(num1 + ", " + num3 + ", " + num2);
+    }
+}
+else if (num3 < num1 && num3 < num2){
+    
+    if(num2 < num1)
+    {
+        console.log(num2 + ", " + num1 + ", " + num3);
+    }
+    else
+    {
+        console.log(num1 + ", " + num2 + ", " + num3);
+    }
+}
