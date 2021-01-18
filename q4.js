18ea8106db797e182f6639600ad8f0be8601f1d9
maayantav <77383039+maayantav@users.noreply.github.com>
2021-01-18 13:03:21 +0200

    Add files via upload

---
+++ q4.js
@@ -0,0 +1,19 @@
+let num1 = 3, num2 = 0, num3 = -4, num4 = 34, num5 = 5;
+let maxNum = num1;
+
+if (num2 > maxNum)
+{
+    maxNum = num2;
+}
+if (num3 > maxNum)
+{
+    maxNum = num3;
+}if (num4 > maxNum)
+{
+    maxNum = num4;
+}if (num5 > maxNum)
+{
+    maxNum = num5;
+}
+
+alert(maxNum);
